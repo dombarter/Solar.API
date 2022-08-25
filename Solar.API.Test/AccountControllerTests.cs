37d9e56bc487ebce4ef3b3a621e143ccc9b6f006
Dom Barter <dom.barter@3squared.com>
2022-08-25 14:57:56 +0100

    Add tests

---
+++ Solar.API.Test/AccountControllerTests.cs
@@ -0,0 +1,142 @@
+﻿using System.Net;
+using Solar.API.Test.Common;
+using Solar.DTOs.Inbound;
+using Xunit;
+
+namespace Solar.API.Test
+{
+    public class AccountControllerTests : IntegrationTestsBase
+    {
+        [Fact]
+        public async Task Post_Register_ReturnsOK()
+        {
+            // Arrange
+            var client = GenerateClient();
+
+            // Act
+            var response = await Register(client, new RegisterDto
+            {
+                Email = "dom@email.com",
+                Password = "PA55word#"
+            });
+
+            var body = await response.Content.ReadAsStringAsync();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Post_Register_DuplicateUser_ReturnsBadRequest()
+        {
+            // Arrange
+            var client = GenerateClient();
+
+            // Act
+            var response1 = await Register(client, new RegisterDto
+            {
+                Email = "dom@email.com",
+                Password = "PA55word#"
+            });
+
+            var response2 = await Register(client, new RegisterDto
+            {
+                Email = "dom@email.com",
+                Password = "PA55word#"
+            });
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response1.StatusCode);
+            Assert.Equal(HttpStatusCode.BadRequest, response2.StatusCode);
+        }
+
+        [Fact]
+        public async Task Post_Register_BadPassword_ReturnsBadRequest()
+        {
+            // Arrange
+            var client = GenerateClient();
+
+            // Act
+            var response = await Register(client, new RegisterDto
+            {
+                Email = "user@email.com",
+                Password = "password"
+            });
+
+            // Assert
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Post_Login_ReturnsOK()
+        {
+            // Arrange
+            var client = GenerateClient();
+
+            // Act
+            var register = await Register(client, new RegisterDto
+            {
+                Email = "dom@email.com",
+                Password = "PA55word#"
+            });
+
+            var login = await Login(client, new LoginDto
+            {
+                Email = "dom@email.com",
+                Password = "PA55word#"
+            });
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, register.StatusCode);
+            Assert.Equal(HttpStatusCode.OK, login.StatusCode);
+        }
+
+        [Fact]
+        public async Task Post_Login_IncorrectEmail_ReturnsBadRequest()
+        {
+            // Arrange
+            var client = GenerateClient();
+
+            // Act
+            var register = await Register(client, new RegisterDto
+            {
+                Email = "dom@email.com",
+                Password = "PA55word#"
+            });
+
+            var login = await Login(client, new LoginDto
+            {
+                Email = "user@email.com",
+                Password = "PA55word#"
+            });
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, register.StatusCode);
+            Assert.Equal(HttpStatusCode.BadRequest, login.StatusCode);
+        }
+
+        [Fact]
+        public async Task Post_Login_IncorrectPassword_ReturnsBadRequest()
+        {
+            // Arrange
+            var client = GenerateClient();
+
+            // Act
+            var register = await Register(client, new RegisterDto
+            {
+                Email = "dom@email.com",
+                Password = "PA55word#"
+            });
+
+            var login = await Login(client, new LoginDto
+            {
+                Email = "dom@email.com",
+                Password = "password"
+            });
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, register.StatusCode);
+            Assert.Equal(HttpStatusCode.BadRequest, login.StatusCode);
+        }
+    }
+}
---
+++ Solar.API.Test/MoonControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using Solar.API.Test.Common;
+using Xunit;
+
+namespace Solar.API.Test
+{
+    public class MoonControllerTests : IntegrationTestsBase
+    {
+        [Theory]
+        [InlineData("/moons/one")]
+        [InlineData("/moons/two")]
+        [InlineData("/moons/user")]
+        public async Task Get_WhenUnauthenticated_ReturnsUnauthorized(string url)
+        {
+            // Arrange
+            var client = GenerateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_OneMoon_AsUser()
+        {
+            // Arrange
+            var client = GenerateClient();
+            await AuthenticateAsUser(client);
+
+            // Act
+            var response = await client.GetAsync("/moons/one");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_TwoMoons_AsUser()
+        {
+            // Arrange
+            var client = GenerateClient();
+            await AuthenticateAsUser(client);
+
+            // Act
+            var response = await client.GetAsync("/moons/two");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Forbidden, response.StatusCode);
+        }
+    }
+}
